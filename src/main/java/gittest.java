9c72cd52f541a347a27d9771e29149ac63a3bd08
clh <123@qq.com>
2022-03-17 23:39:31 +0800

    hot-fix commit1

--- src/main/java/gittest.java
+++ src/main/java/gittest.java
@@ -3,5 +3,6 @@ public class gittest {
         System.out.printf("hello git");
         System.out.printf("hello git2");
         System.out.printf("hello git3");
+        System.out.printf("hot-fix");
     }
 }
