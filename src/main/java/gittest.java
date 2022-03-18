d0dec9467df4f3d3e7042c85d1bc4bb7052f428e
clh <123@qq.com>
2022-03-18 10:36:28 +0800

    master commit2

--- src/main/java/gittest.java
+++ src/main/java/gittest.java
@@ -2,10 +2,6 @@ public class gittest {
     public static void main(String[] args) {
         System.out.printf("hello git");
         System.out.printf("hello git2");
-        System.out.printf("hello git3");
-        System.out.printf("hello git4");
-        System.out.printf("master test");
-        System.out.printf("hello git4");
-        System.out.printf("hot-fix test");
+        System.out.printf("hello git88");
     }
 }
